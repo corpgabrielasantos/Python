6663039f21fe3f6bf2bb6301b59c8bf7bb95e105
Gabriela Santos <gabi.carmo.santos1@gmail.com>
2026-01-09 12:32:24 -0300

    Update pratica1.py

--- pratica1.py
+++ pratica1.py
@@ -5,8 +5,12 @@
 
 # Desafio 1 
 
-# nome =  input("Olá, Pode me informar seu nome: ")
-# print(f"Seja bem vindo(a) {nome}, é um prazer te ter aqui.")
+nome =  input("Olá, Pode me informar seu nome: ")
+print(f"Seja bem vindo(a) {nome}, é um prazer te ter aqui.")
 
 # Desafio 2
 
+dia = int(input(f"{nome}, pode informar o dia do seu nascimento? "))
+mes = int(input(f"Agora {nome}, me diz o mes: "))
+ano = int(input("Para finalizar o ano: "))
+print(f"Então vamos verificar, seu nome é {nome} e sua data de nacimento é {dia}/{mes}/{ano}.")
