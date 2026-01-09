0e3210599c54409e562076ea8cff259b1b02a84e
Gabriela Santos <gabi.carmo.santos1@gmail.com>
2026-01-09 12:24:46 -0300

    Create pratica1.py

---
+++ pratica1.py
@@ -0,0 +1,12 @@
+# nome = input("Qual seria seu nome: ")
+# idade = input("Agora digite sua idade: ")
+# peso = input("E qual sria seu peso: ")
+# print(f"Então seu nome é {nome} com a idade de {idade} anos e peso de {peso}kg ")
+
+# Desafio 1 
+
+# nome =  input("Olá, Pode me informar seu nome: ")
+# print(f"Seja bem vindo(a) {nome}, é um prazer te ter aqui.")
+
+# Desafio 2
+
