aee4770b1fc5f54660794e91198b68b818c78e48
Gabriela Santos <gabi.carmo.santos1@gmail.com>
2026-01-16 15:35:38 -0300

    Create exercicio3.py

---
+++ exercicio3.py
@@ -0,0 +1,15 @@
+# n1 = int(input("Digite o valor que deseja, por favor: "))
+# n2 = int(input("Agora digite outro: "))
+# s = n1 + n2
+# print(f"O total é: {s} ")
+
+varia = input("Digite um valor: ")
+print("O tipo primitivo é:5", type(varia))
+print("Só tem espaços?", varia.isspace())
+print("É um número:", varia.isnumeric())
+print("É alfabético?", varia.isalpha())
+print("É alfanúmerico?", varia.isalnum())
+print("Está em maiúsculo?",varia.isupper())
+print("Está em minúsculo",varia.islower())
+print("Está capitalizado",varia.istitle())
+
