be1279785bc5c0ed5a40c060ce6cb8274ac2a654
Gabriela Santos <gabi.carmo.santos1@gmail.com>
2026-01-19 16:14:52 -0300

    Create exercício4.py

---
+++ exercício4.py
@@ -0,0 +1,37 @@
+# n = int(input("Digite um número: "))
+# s = 1 + n
+# sub = n - 1
+# print(f"O sucessor de {n} é {s} e o antecessor é {sub}")
+
+# mult = n ** 2
+# mult3 = n ** 3 
+# raiz = n ** 0.5
+# print(f"O dobro de {n} é {mult}, o triplo é {mult3} e a raiz é {raiz}")
+
+# n1 = int(input("Ola, vamos calcular sua média? Digite a primeira nota, por favor: "))
+# n2 = int(input("Agora digite a segunda nota: "))
+# média = n1 + n2/2
+# print(f"Sua média é {média}")
+
+# n = int(input("Digite um número: "))
+# cent = n * 100
+# mili = n * 1000
+# print(f"São {cent} centímetros e {mili} milímetros")
+
+# n = int(input("Qual tabuada você deseja? "))                 [xx]
+# tabuada = n * 1
+# print(f"{tabuada}")
+
+# dinheiro = int(input("Quanto deseja converter? "))
+# dolar = dinheiro * 3.7
+# print(f"Você pode comprar {dolar} doláres.")
+
+# n1 = int(input("Qual a largura do seu cômodo? "))
+# n2 = int(input("Qual o altura do seu cômodo? "))
+# area = n1 * n2 / 2                                                   [xx]
+# tinta = area * 2
+# print(f"A quantidade de tinta necessária para pintar a parede é {tinta} litros")
+
+# preço = int(input("Qual valor do produto? "))
+# novo = preço * (1 + 5 / 100)
+# print(f"O novo preço é {novo} reais.")
