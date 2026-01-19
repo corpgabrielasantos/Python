5592fe6adccf034526ad0184865a6e8ebbbd96ff
Gabriela Santos <gabi.carmo.santos1@gmail.com>
2026-01-19 16:47:51 -0300

    Update exercício4.py

--- exercício4.py
+++ exercício4.py
@@ -1,16 +1,16 @@
 # n = int(input("Digite um número: "))
 # s = 1 + n
 # sub = n - 1
-# print(f"O sucessor de {n} é {s} e o antecessor é {sub}")
+# print(f"O sucessor de {n} é {s} e o antecessor é {sub}") 
 
-# mult = n ** 2
-# mult3 = n ** 3 
+# mult = n * 2
+# mult3 = n * 3 
 # raiz = n ** 0.5
 # print(f"O dobro de {n} é {mult}, o triplo é {mult3} e a raiz é {raiz}")
 
 # n1 = int(input("Ola, vamos calcular sua média? Digite a primeira nota, por favor: "))
 # n2 = int(input("Agora digite a segunda nota: "))
-# média = n1 + n2/2
+# média = (n1 + n2)/2
 # print(f"Sua média é {média}")
 
 # n = int(input("Digite um número: "))
@@ -18,20 +18,29 @@
 # mili = n * 1000
 # print(f"São {cent} centímetros e {mili} milímetros")
 
-# n = int(input("Qual tabuada você deseja? "))                 [xx]
-# tabuada = n * 1
-# print(f"{tabuada}")
+# n = int(input("Qual tabuada você deseja? "))               
+# print("{} X {} = {} ".format(n, 1, n*1))
+# print("{} X {} = {} ".format(n, 2, n*2))
+# print("{} X {} = {} ".format(n, 3, n*3))
+# print("{} X {} = {} ".format(n, 4, n*4))
+# print("{} X {} = {} ".format(n, 5, n*5))
+# print("{} X {} = {} ".format(n, 6, n*6))
+# print("{} X {} = {} ".format(n, 7, n*7))
+# print("{} X {} = {} ".format(n, 8, n*8))
+# print("{} X {} = {} ".format(n, 9, n*9))
+# print("{} X {} = {} ".format(n, 10, n*10))
 
 # dinheiro = int(input("Quanto deseja converter? "))
-# dolar = dinheiro * 3.7
+# dolar = dinheiro / 3.7
 # print(f"Você pode comprar {dolar} doláres.")
 
 # n1 = int(input("Qual a largura do seu cômodo? "))
 # n2 = int(input("Qual o altura do seu cômodo? "))
-# area = n1 * n2 / 2                                                   [xx]
-# tinta = area * 2
-# print(f"A quantidade de tinta necessária para pintar a parede é {tinta} litros")
+# area = n1 * n2                                                 
+# print(f"A sua are é {area}")
+# tinta = area / 2
+# print(f"Para pintar precisa de {tinta} litros.")
 
 # preço = int(input("Qual valor do produto? "))
-# novo = preço * (1 + 5 / 100)
+# novo = preço -(preço * 5/ 100)
 # print(f"O novo preço é {novo} reais.")
