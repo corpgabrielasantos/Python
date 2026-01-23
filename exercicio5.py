3fa344053290acc95d04c75d327280d0417fdb5b
Gabriela Santos <gabi.carmo.santos1@gmail.com>
2026-01-23 14:31:28 -0300

    Create exercicio5.py

---
+++ exercicio5.py
@@ -0,0 +1,46 @@
+# import math
+# nun =  float(input("Digite um número: "))
+# nun2 = math.floor(nun)
+# print(nun2)
+
+# from math import trunc
+# nun = float(input("Digite um valor: "))
+# print(f"O valor digitado foi {nun} e sua porção inteita é {trunc(nun)}")
+# =============
+
+# import math
+# oposto =  int(input("Digite o cateto oposto: "))
+# adjacente = int(input("Digite o cateto da ipotenusa: "))
+# hipotenusa = math.hypot(oposto, adjacente)
+# print(f"A Hipotenusa é igual a {hipotenusa}")
+
+# co = float(input("Comprimento do cateto oposto: "))
+# ca = float(input("Comprimento do cateto adjacente: "))
+# hipo = (co ** 2 + ca ** 2) ** (1/2)
+# print(f"A hipotenusa vai medir {hipo}")
+# =============
+
+# from math import sin, cos, tan, radians
+# angulo = radians(float(input("Quantos graus você quer o resultado? ")))
+# print(sin(angulo))
+# print(cos(angulo))
+# print(tan(angulo))
+
+# import random
+# m1 = str(input("Primeiro Aluno: "))
+# m2 = str(input("Segundo Aluno: "))
+# m3 = str(input("Terceiro Aluno: "))
+# m4 = str(input("Quarto Aluno: "))
+# lista = [m1, m2, m3, m4]
+# escolhido = random.choice(lista)
+# print(f"O Aluno escolhido foi {escolhido}")
+
+# import random
+# m1 = str(input("Primeiro Aluno: "))
+# m2 = str(input("Segundo Aluno: "))
+# m3 = str(input("Terceiro Aluno: "))
+# m4 = str(input("Quarto Aluno: "))
+# lista = [m1, m2, m3, m4]
+# random.shuffle(lista)
+# print("A ordem de apresentação sera")
+# print(lista)
