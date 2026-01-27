9fe0ac0f26f5f0819fd418cfea7bfe1b1d7e8bfb
Gabriela Santos <gabi.carmo.santos1@gmail.com>
2026-01-27 15:34:59 -0300

    Comment out unused code in exercicio6.py

--- exercicio6.py
+++ exercicio6.py
@@ -6,6 +6,11 @@
 
 # num = int(input("Digite um número de 0 a 9999: "))
 
+# print(n[3])
+# print(n[2])
+# print(n[1])
+# print(n[0])
+
 # unidade = num // 1 % 10
 # dezena = num // 10 % 10
 # centena = num // 100 % 10
