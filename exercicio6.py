67c08a801bcd805ff8da2f6a5757db6643c3cde4
Gabriela Santos <gabi.carmo.santos1@gmail.com>
2026-01-27 13:35:10 -0300

    Create exercicio6.py

---
+++ exercicio6.py
@@ -0,0 +1,35 @@
+# nome = input("Digite seu nome completo: ")
+# print(nome.upper())
+# print(nome.lower())
+# print(len(nome.replace(" ","")))
+# print(len(nome.split()[0]))
+
+# num = int(input("Digite um número de 0 a 9999: "))
+
+# unidade = num // 1 % 10
+# dezena = num // 10 % 10
+# centena = num // 100 % 10
+# milhar = num // 1000 % 10
+
+# print("Unidade:", unidade)
+# print("Dezena:", dezena)
+# print("Centena:", centena)
+# print("Milhar:", milhar)
+
+# cidade = input("Digite o nome da sua cidade: ")
+# print("Santo" in cidade.lstrip())
+
+# nome = input("Digite o nome da sua cidade: ")
+# print("Santos" in nome)
+
+# nome = input("Digite o nome: ")
+# print(nome.count("a"))
+# print(nome.find("a"))
+# print(nome.rfind("a"))
+
+# frase = input("Digite seu nome completo: ")
+
+# palavras = frase.split()
+
+# print("Primeira palavra:", palavras[0])
+# print("Última palavra:", palavras[-1])
