8296df254446cf61673cd6b38054f1835b342b12
Gabriela Santos <gabi.carmo.santos1@gmail.com>
2026-01-27 12:38:46 -0300

    Create pratica4.py

---
+++ pratica4.py
@@ -0,0 +1,45 @@
+# # frase = "Curso em vídeo Python"
+# # print(frase)
+
+# # frase = "Curso em vídeo Python"
+# # print(frase[3])
+
+# # frase = "Curso em vídeo Python"
+# # print(frase[3:13])
+
+# # frase = "Curso em vídeo Python"
+# # print(frase[:13])
+
+# # print(frase[1:15:2])
+
+# print(frase[::2])
+
+# print(frase.count("o"))
+
+# print(frase.upper().count("o"))
+
+# print(len(frase))
+
+# print(len(frase.strip()))
+
+# print(frase.replace("Python", "Android"))
+
+# print(frase.lower().find("vídeo"))
+
+# frase.replace("Python"."Android")
+# print(frase)
+
+# frase = frase.replace("Python"."Android")
+# print(frase)
+
+# print("Curso" in frase)
+
+# print(frase.find("vídeo"))
+
+# print(frase.split())
+
+# dividido = frase.split()
+# print(dividido)
+
+# dividido = frase.split()
+# print(dividido[0])
