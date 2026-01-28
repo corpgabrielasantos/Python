3b0e7f16f347f65a4933fc5ba225605a6561f572
Gabriela Santos <gabi.carmo.santos1@gmail.com>
2026-01-28 13:44:50 -0300

    Create pratica5.py

---
+++ pratica5.py
@@ -0,0 +1,27 @@
+# tempo = int(input("Quantos anos tem seu carro? "))
+# if tempo <= 3:
+#     print("Seu carro é novo")
+# else:
+#     print("Seu carro ja passou da idade de 3 anos")
+# print("--FIM--")
+
+# nome = str(input("Qual seu nome: "))
+# if nome == 'Gustavo':
+#     print("Que nome lindo você tem!")
+# print(f"Bom dia {nome}!")
+
+# nome = str(input("Qual seu nome: "))
+# if nome == 'Gustavo':
+#     print("Que nome lindo você tem!")
+# else:
+#     print("Seu nome é tão normal!")
+# print(f"Bom dia, {nome}!")
+
+# n1 = float(input("Digite a primeira nota: "))
+# n2 = float(input("Digite a segunda nota: "))
+# m = (n1 + n2)/2
+# print(f"A sua média foi {m}")
+# if m >= 6.0:
+#     print("Sua média foi boa! PARABÉNS!")
+# else:
+#     print("Sua média foi ruim! ESTUDE MAIS!")
